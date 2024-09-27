e299e8d183604a4d1ea401083d820a3e06b89ad7
Sameeu20 <abdulsameeu9@gmail.com>
2024-09-28 03:06:29 +0530

    Add Cloud Gateway

---
+++ backend/gateway-service/src/test/java/com/parenteye/gateway_service/GatewayServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.parenteye.gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
