1959ad33e50ca6df7ef8e784e7225a3b95a67d06
Sameeu20 <abdulsameeu9@gmail.com>
2024-09-28 00:54:27 +0530

    Add Assignment Dashboard

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -4,10 +4,11 @@ import { LoginComponent } from './login/login.component';
 import { ParentDashboardComponent } from "./parent-dashboard/parent-dashboard.component";
 import { TeacherDashboardComponent } from './teacher-dashboard/teacher-dashboard.component';
 import { ContentmanagementComponent } from './contentmanagement/contentmanagement.component';
+import { AssignmentDashboardComponent } from './assignment-dashboard/assignment-dashboard.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, ParentDashboardComponent,TeacherDashboardComponent,ContentmanagementComponent],
+  imports: [RouterOutlet, LoginComponent, ParentDashboardComponent,TeacherDashboardComponent,ContentmanagementComponent,AssignmentDashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
